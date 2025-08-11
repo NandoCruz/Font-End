3c3891e8fc759b093deac63997c7b04d9241e6f9
NandoCruz <nando33_2005@hotmail.com>
2025-08-11 12:53:46 -0300

    Funções do Javascript
    Ajustando as funcionalidades do Javascript do button Leia mais.

--- Aula01/curriculum01/scripts.js
+++ Aula01/curriculum01/scripts.js
@@ -1,31 +1,37 @@
 function leiaMais(){
     let pontos=document.getElementById("pontos");
     let maistexto=document.getElementById("mais");
+    let leiaMais=document.getElementById("leiaMais");
     let btnLeiaMais=document.getElementById("btnLeiaMais");
 
     if(pontos.style.display === "none"){
         pontos.style.display="inline";
         maistexto.style.display="none";
+        leiaMais.style.height="150px";
         btnLeiaMais.innerHTML="Leia Mais";
     } else {
         pontos.style.display="none";
         maistexto.style.display="inline";
+        leiaMais.style.height="670px";
         btnLeiaMais.innerHTML="Leia Menos";
     }
 }
 
-function leiaMaisEx(){
-    let pontos=document.getElementById("pontos");
-    let maistexto=document.getElementById("mais");
-    let btnLeiaMais=document.getElementById("btnLeiaMais");
+function leia(){
+    let pontosEx=document.getElementById("pontosEx");
+    let verMaisEx=document.getElementById("verMais");
+    let leiaMaisEx=document.getElementById("leiaMaisEx");
+    let btnLeiaMaisEx=document.getElementById("btnLeiaMaisEx");
 
-    if(pontos.style.display === "none"){
-        pontos.style.display="inline";
-        maistexto.style.display="none";
-        btnLeiaMais.innerHTML="Leia Mais";
+    if(pontosEx.style.display === "none"){
+        pontosEx.style.display="inline";
+        verMaisEx.style.display="none";
+        leiaMaisEx.style.height="600px";
+        btnLeiaMaisEx.innerHTML="Leia Mais";
     } else {
-        pontos.style.display="none";
-        maistexto.style.display="inline";
-        btnLeiaMais.innerHTML="Leia Menos";
+        pontosEx.style.display="none";
+        verMaisEx.style.display="inline";
+        leiaMaisEx.style.height="1040px";
+        btnLeiaMaisEx.innerHTML="Leia Menos";
     }
 }
